a557ee1e320c4855e3fa5d0f360688301b726243
park kevin <parkkevin@parkdeAir-M1.lan>
2022-01-19 12:19:46 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- codeing = utf-8 -*-
+# @time :2022/1/1911:13
+# @Author : park
+# @File :example_demo.py
+# @Software:PyCharm
+
+import sys
+import example_files
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+
+    ui = example_files.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    MainWindow.show()
+    sys.exit(app.exec_())
